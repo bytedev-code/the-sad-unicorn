6cf7ab87947ebffdda51f33fae53b37f2fb30a37
Patrick Baudach <patrick@baudach.eu>
2024-01-26 21:06:49 +0100

    implement signal for hp and lost

--- player.gd
+++ player.gd
@@ -1,11 +1,21 @@
 extends CharacterBody2D
 
-const SPEED = 300.0
+signal on_damage(hp: int)
+signal on_lost()
+
+@export var SPEED: float = 300.0
 @export var INIT_HP: int = 3
+
 var _hp = 3
+var _is_invincible = false
+
+@onready var InvincibleTimer = $InvincibleTimer
 
 func _ready():
 	_hp = INIT_HP
+	_is_invincible = true
+	InvincibleTimer.timeout.connect(_on_invincible_gone)
+	InvincibleTimer.start()
 
 func _physics_process(delta):
 	
@@ -21,3 +31,20 @@ func _physics_process(delta):
 		rotate(0.1)
 
 	move_and_slide()
+	
+	if get_slide_collision_count() > 0 and not _is_invincible:
+		var collider = get_slide_collision(0)
+		_get_damage()
+
+func _get_damage():
+	_hp -= 1
+	_is_invincible = true
+	InvincibleTimer.start()
+	
+	on_damage.emit(_hp)
+	
+	if _hp <= 0:
+		on_lost.emit()
+		
+func _on_invincible_gone():
+	_is_invincible = false
