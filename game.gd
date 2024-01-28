139b0f642ef0ebc8fc19bb779c35ca4e85315a57
manschko <manschko1999@googlemail.com>
2024-01-28 13:26:51 +0100

    added Particles achivement

--- game.gd
+++ game.gd
@@ -1,4 +1,4 @@
-class_name GameManager
+class_name GameManager 
 extends Node2D
 
 signal on_gamestart(mode: String)
--- ui/scripts/hud.gd
+++ ui/scripts/hud.gd
@@ -43,21 +43,4 @@ func death(enemy: String):
 	
 func touchedEnemy(enemy: String, health:int):
 	$Achivements.touchedEnemy(enemy, health)
-	
-func _input(ev):
-	if Input.is_key_pressed(KEY_K):
-		addEnemyDeath("bannane")
-	elif  Input.is_key_pressed(KEY_T):
-		gameLost("default")
-	elif Input.is_key_pressed(KEY_O):
-		$Achivements.displayAchivment({
-		"name":"The Cake is a Lie",
-		"texture": preload("res://assets/enemies/cake.png"), 
-		"reference":["enemies","damageTaken","cake"], 
-		"goal":1, 
-		"reached": false
-		}
-		)
-	elif Input.is_key_pressed(KEY_R):
-		$Achivements.resetSave()
 		
